759976a1db743bc9adad81961d7978f349e0435c
Megan Davis <davisma24@vcu.edu>
2025-05-06 15:40:27 -0400

    Added doc strings to all the methods.

--- breast_cancer_classification/logistic_regression_model.py
+++ breast_cancer_classification/logistic_regression_model.py
@@ -9,8 +9,14 @@ from sklearn.preprocessing import StandardScaler
 
 
 def load_data(data_filepath: str, debug: bool = False) -> pd.DataFrame:
-    """
+    """Loads the .csv file into a dataframe.
 
+    Args:
+        data_filepath: The file path for the csv.
+        debug: The option to turn on printing for dataframe information.
+
+    Returns:
+        The new dataframe.
     
     """
     
@@ -28,7 +34,13 @@ def load_data(data_filepath: str, debug: bool = False) -> pd.DataFrame:
 
 #Pass by Object Reference >> Dataframes are fine to pass and not return
 def preprocess_data(data: pd.DataFrame):
-    """
+    """Preprocess the data in the dataframe.
+
+    Args:
+        data: The dataframe containing the data.
+        
+    Returns:
+        None.
     
     """
     # Convert 'diagnosis' column to numeric (e.g., 'B' -> 0, 'M' -> 1)
@@ -36,7 +48,14 @@ def preprocess_data(data: pd.DataFrame):
 
 
 def create_test_train_split(data: pd.DataFrame, debug: bool = False):
-    """
+    """Create the test train split for the data.
+
+    Args:
+        data: The dataframe containing the data.
+        debug: The option to turn on printing information about the training and test shapes.
+        
+    Returns:
+        The X,y train and test splits.
     
     """
     X = data.drop(['diagnosis', 'id'], axis=1)
@@ -53,7 +72,14 @@ def create_test_train_split(data: pd.DataFrame, debug: bool = False):
     return X_train, X_test, y_train, y_test
 
 def scale_data(X_train, X_test):
-    """
+    """Scales the X train and X_Test data to have a mean of 0 and standard deviation of 1.
+
+    Args:
+        X_train : The X training dataset.
+        X_test: The X test dataset.
+        
+    Returns:
+        The X_train and X_test data scaled.
     
     """
     # Scale features to ensure each has a mean of 0 and stdv of 1
@@ -64,8 +90,14 @@ def scale_data(X_train, X_test):
     return X_train_scaled, X_test_scaled
 
 
-def create_lr_model(max_iter: int = 100):
-    """
+def create_lr_model(max_iter: int = 100) -> LogisticRegression:
+    """Creates the logistic regression model.
+
+    Args:
+        max_iter : The max number of iterations before converging on the best solution.
+        
+    Returns:
+        The logistic regression model.
     
     """
     # Initialize logistic regression model
@@ -74,15 +106,32 @@ def create_lr_model(max_iter: int = 100):
    
 
 def fit_lr_model(lr_model: LogisticRegression, X_train, y_train):
-    """
+    """Fits the logistic regression model based on the X_train and y_train data.
+
+    Args:
+        lr_model : The logisitic regression model.
+        X_train : The X training dataset.
+        y_train : The y training dataset.
+        
+    Returns:
+        None
     
     """
     
     lr_model.fit(X_train, y_train)
     
 
-def evaluate_lr_model(lr_model: LogisticRegression, X_train, X_test, y_train, y_test):
-    """
+def evaluate_lr_model(lr_model: LogisticRegression, X_test, y_test):
+    """Evalutes the accuracy of logistic regression model and prints the results.
+
+    Args:
+        lr_model : The logisitic regression model.
+        X_train : The X training dataset.
+        y_train : The y training dataset.
+        
+    Returns:
+        The y_predictions, accuracy, confusion matrix, and the classification report.
+    
     
     """
     # Make predictions
@@ -96,13 +145,23 @@ def evaluate_lr_model(lr_model: LogisticRegression, X_train, X_test, y_train, y_
     print(f"Confusion Matrix:\n {conf_matrix}")
     print(f"Classification Report:\n {class_report}")
 
-def generate_feature_importance(lr_model:LogisticRegression, X):
-    """
+    return y_pred, accuracy, conf_matrix, class_report
+
+def generate_feature_importance(lr_model:LogisticRegression, original_df: pd.DataFrame) -> pd.DataFrame:
+    """Generates the feature importance report and outputs it.
+
+    Args:
+        lr_model : The logisitic regression model.
+        original_df: The original dataframe with all the columns.
+        
+    Returns:
+        The important features dataframe.
+    
     
     """
     # Extract feature importance
     # Extract feature names from the dataset
-    feature_names = X.columns
+    feature_names = original_df.columns
     # Get the coefficients
     coefficients = lr_model.coef_[0]  # shape (n_features,)
 
@@ -119,16 +178,38 @@ def generate_feature_importance(lr_model:LogisticRegression, X):
     print("Feature Importance Data:")
     print(feature_importance)
 
+    return feature_importance
+
 
 def save_trained_model(lr_model:LogisticRegression, file_name: str):
-    """
+    """Save the trained model to the /models directory.
+
+    Args:
+        lr_model : The logisitic regression model.
+        file_name : The name of the output file.
+        
+    Returns:
+        None.
+    
     
     """
-    with open(f"../models/", 'wb') as file:
+    with open(f"../models/{file_name}", 'wb') as file:
         pickle.dump(lr_model, file)
 
 
-def load_lr_model(lr_model: LogisticRegression, filepath: str):
+def load_lr_model(filepath: str):
+    """Load an already existing model from a pickle file.
+
+    Args:
+        file_path : The location of the logistic regression pickle file.
+        
+    Returns:
+        Loaded model from file.
+    
+    
+    """
     # Load the model   
     with open(filepath, 'rb') as file:
         lr_model = pickle.load(file)
+
+    return lr_model
