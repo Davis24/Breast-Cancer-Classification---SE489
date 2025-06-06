eb8fe3861018097a46426fa79381a5d2c97f2d7f
Megan Davis <davisma24@vcu.edu>
2025-06-06 13:30:32 -0400

    Updated app.py

--- app/app.py
+++ app/app.py
@@ -1,18 +1,21 @@
+import pickle
+
 from fastapi import FastAPI
 import numpy as np
-import pickle
 
 app = FastAPI()
 
 # Load the trained model
-lr_model = pickle.load('../models/model.pkl')
+lr_model = pickle.load("../models/model.pkl")
+
 
 @app.get("/")
 def read_root():
     return {"message": "Welcome to Breast Cancer Classification - SE 489 Model"}
 
+
 @app.post("/predict/")
 def predict(data: dict):
-    X_test = np.array(data['features'])
+    X_test = np.array(data["features"])
     y_pred = lr_model.predict(X_test)
-    return {"predictions" : y_pred}
+    return {"predictions": y_pred}
